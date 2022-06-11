fde7717c85b8768e717c3ed79804ebdc91197ed1
mavrag <vadim.psc@gmail.com>
2022-06-11 20:21:53 +0000

    Updated Scripts

---
+++ scripts/make-links.sh
@@ -0,0 +1,7 @@
+rm ~/.bash*
+ln -s ~/mav_linux_conf/.bash_profile ~/.bash_profile
+ln -s ~/mav_linux_conf/.bashrc ~/.bashrc
+rm ~/.zshrc
+ln -s ~/mav_linux_conf/.zshrc ~/.zshrc
+rm ~/.p10k.zsh
+ln -s ~/mav_linux_conf/.p10k.zsh ~/.p10k.zsh
